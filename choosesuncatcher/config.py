543b4146aed15415e5c1524f91714068fde55379
kotbarmaglot <115881366+kotbarmaglot@users.noreply.github.com>
2023-04-23 15:28:46 +0300

    Delete config.py

--- choosesuncatcher/config.py
+++
@@ -1,7 +0,0 @@
-# бот для покупки ловцов
-
-URL_BOT = 'http://t.me/choosesuncatcherbot' 
-API_KEY = '5761031821:AAG78NzPFUqlEj4bwCsOOyzbX7IJ9ukN6fk'
-
-VK_MARKET_ID = "-211053112"
-VK_TOKEN_USER = "vk1.a.mU2-fPvBrU6lB_AUkTutul5o4Cdnk1unXoPVP8IDoU-BfVRa6euC-yfgMhmhu7_ZHcKWltAzpY9NKg7fsQon6-oK0YPMXrLIsK9Ibmh71xevMkubN4CehhQNn7OvS5X6D07mjf-BMmnqsSUjMUGrdO_b3wvSHAPrOdAbJFPwgSxl55Xfh6ukXvjZBNW2KwGkSZV-NFF7BcAd8SJKvE1XEw"
